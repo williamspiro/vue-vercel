4e9bbc03b8784c8a4089f38440e9d98074562739
williamspiro <willspiro@gmail.com>
2020-08-03 11:28:07 -0400

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2>Here are some changes I am making in a branch</h2>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
